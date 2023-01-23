fc57919b6d47a3879f3433511b21f7b541516855
rayan <rayanidrees4@gmail.com>
2023-01-23 05:04:56 +0500

    second push of first roadmap updated

---
+++ src/components/roadmap.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/jsx-no-duplicate-props */
+
+
+function Roadmap() {
+
+return (
+
+            
+            <><div className="xl:w-[100%] lg:w-[100] 2xl:w-[100] ">
+
+        <div className="   absolute roadmap-bg">
+            <img className=" opacity-100 z-100" src="./assets/texture-1.png" alt="texture" />
+            <h1 className=" opacity-70 lg:text-center  absolute xl:top-0 2xl:top-0 2xl:mx-[32.2%]  xl:mx-[23.9%] batsand "> THE TURTLES <br /> ROAD MAP &nbsp;  </h1>
+
+
+            <div className="absolute box-1  border xl:w-[70%] 2xl:w-[70%] 2xl:h-[30%] 2xl:top-[46%] 2xl:mx-[15.9%] xl:h-[30%]  opactiy-80 xl:top-[66%]  xl:mx-[13.9%]">
+
+                
+                        <div className=" xl:text-center 2xl:text-center ">
+
+                                <h4 className="dummy-1 opacity-80 xl:my-[2%]">dummy text</h4>
+                                <h6 className="inner-dummy"> Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text </h6>
+                        </div>
+                        
+            </div>
+        </div>
+
+
+
+
+
+
+    </div>
+   
+        </>
+                
+         
+             
+             
+
+
+
+
+)}
+
+
+export default Roadmap;
