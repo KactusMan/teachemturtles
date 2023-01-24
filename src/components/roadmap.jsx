e39746facaebf6b016c33e4f39c73e2da87662f7
rayan <rayanidrees4@gmail.com>
2023-01-24 23:21:23 +0500

    roadmap section done for xl and 2xl without vectors

--- src/components/roadmap.jsx
+++ src/components/roadmap.jsx
@@ -6,31 +6,107 @@ function Roadmap() {
 return (
 
             
-            <><div className="xl:w-[100%] lg:w-[100] 2xl:w-[100] ">
+            <><div className="xl:w-[100%] lg:w-[100%] 2xl:w-[100%]   ">
 
-        <div className="   absolute roadmap-bg">
-            <img className=" opacity-100 z-100" src="./assets/texture-1.png" alt="texture" />
-            <h1 className=" opacity-100 lg:text-center z-20  absolute xl:top-0 2xl:top-0 2xl:mx-[32.2%] 2xl:mt-40    xl:mx-[23.9%] batsand "> THE TURTLES <br /> ROAD MAP &nbsp;  </h1>
-            <h1 className=" opacity-100 lg:text-center z-10  absolute xl:top-0 2xl:top-0 2xl:mx-[31.8%] 2xl:mt-40 2xl:top-4  xl:mx-[23.4%] batsand-dark"> THE TURTLES <br /> ROAD MAP &nbsp;  </h1>
 
-            <div className="absolute box-1  border xl:w-[70%] 2xl:w-[70%] 2xl:h-[30%] 2xl:top-[46%] 2xl:mt-40 2xl:mx-[15.9%] xl:h-[30%]  opactiy-80 xl:top-[66%]  xl:mx-[13.9%]">
+
+        <div className="   absolute roadmap-bg xl:h-[350%] 2xl:h-[280%] ">
+            <img className="  opacity-100 z-50  " src="./assets/texture-1.png" alt="texture" />
+            <img className=" absolute xl:-z-0 xl:-my-[30%] 2xl:-z-0 2xl:-my-[36%] 2xl:w-[40%] " src="./assets/left-tree.png" alt="left-tree" />
+            <img className=" absolute xl:-z-0  xl:mx-[60%] xl:-my-[10%] 2xl:-z-0 2xl:mx-[54%] 2xl:my-[-13%] 2xl:w-[46%] " src="./assets/right-tree.png" alt="right-tree" />
+            
+            <h1 className=" opacity-100 lg:text-center z-30  absolute xl:top-[4%] 2xl:top-0 2xl:mx-[32.2%] 2xl:mt-40    xl:mx-[23.9%] batsand "> THE TURTLES <br /> ROAD MAP &nbsp;  </h1>
+            <h1 className=" opacity-100 lg:text-center z-20  absolute xl:top-[4%]  2xl:mx-[31.8%] 2xl:mt-40 2xl:top-4  xl:mx-[23.4%] batsand-dark"> THE TURTLES <br /> ROAD MAP &nbsp;  </h1>
+
+            <div className="absolute box-1   border xl:w-[70%] 2xl:w-[70%] 2xl:h-[10%] 2xl:top-[22%] 2xl:mt-40 2xl:mx-[15.9%] xl:h-[12%] xl:top-[28.5%]  div-road   z-10   xl:mx-[13.9%]">
 
                 
-                        <div className=" xl:text-center 2xl:text-center ">
+                        <div className=" xl:text-center 2xl:text-center  ">
 
-                                <h4 className="dummy-1 opacity-80 xl:my-[2%] 2xl:my-[4%]">dummy text</h4>
-                                <h6 className="inner-dummy"> Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text </h6>
+                                <h4 className="dummy-1 opacity-70 xl:my-[2%] 2xl:my-[4%]">dummy text</h4>
+                                <h6 className="inner-dummy opacity-70"> Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text </h6>
                         </div>
                         
+
             </div>
+
+            {/* 2nd box */}
+
+            <div className="">
+                    <img className=" opacity-100 z-100 xl:my-[0.01%]  " src="./assets/texture-2.png" alt="texture" />
+                   
+            <div className="absolute box-1  border xl:w-[70%] 2xl:w-[70%] 2xl:h-[10%] 2xl:top-[38%] 2xl:mt-40 2xl:mx-[15.9%] xl:h-[12%] xl:top-[45%]  opactiy-80 div-road  xl:mx-[13.9%]">
+       
+                
+<div className=" xl:text-center 2xl:text-center z-10 ">
+
+        <h4 className="dummy-1  xl:my-[2%] 2xl:my-[4%] opacity-70">dummy text</h4>
+        <h6 className="inner-dummy opacity-70"> Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text </h6>
+</div>
+
+
+</div>
+</div>
+
+
+        {/* 3rd box */}
+                   
+
+        <div className=" ">
+                    <img className=" opacity-100 z-100 xl:my-[0.01%]  " src="./assets/curve-texture.png" alt="texture" />
+                   
+                   
+            <div className="absolute box-1  border xl:w-[70%] 2xl:w-[70%] 2xl:h-[10%] 2xl:top-[53.5%] 2xl:mt-40 2xl:mx-[15.9%] xl:h-[12%] xl:top-[62%]  opactiy-80 div-road  xl:mx-[13.9%]">
+       
+                
+<div className=" xl:text-center 2xl:text-center ">
+
+        <h4 className="dummy-1  xl:my-[2%] 2xl:my-[4%] opacity-70">dummy text</h4>
+        <h6 className="inner-dummy opacity-70"> Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text </h6>
+</div>
+
+
+</div>
+</div>
+
+
+            {/* 4th box */}
+
+
+            <div className="absolute box-1  border xl:w-[70%] 2xl:w-[70%] 2xl:h-[10%] 2xl:top-[68.5%] 2xl:mt-40 2xl:mx-[15.9%] xl:h-[12%] xl:top-[79%]  opactiy-80 div-road  xl:mx-[13.9%]">
+
+                
+<div className=" xl:text-center 2xl:text-center ">
+
+        <h4 className="dummy-1 opacity-70 xl:my-[2%] 2xl:my-[4%]">dummy text</h4>
+        <h6 className="inner-dummy opacity-70"> Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text </h6>
+</div>
+
+
+</div>
+
+
+
+
+      
+        
         </div>
+                
 
 
 
 
-
-
+                 {/* roadmap div closing */}
     </div>
+
+            {/* poker div */}
+
+{/* <div className="absolute bottom-4">
+
+        <h1 className="text-red-600">hello</h1>
+</div> */}
+
+
    
         </>
                 
