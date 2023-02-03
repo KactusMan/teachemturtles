7dc799bc0406882ca904dcc53ebbc6275fcf3ecd
rayan <rayanidrees4@gmail.com>
2023-02-04 02:21:13 +0500

    roadmap section done this bitch better be working fine when i check on my phone

--- src/components/roadmap.jsx
+++ src/components/roadmap.jsx
@@ -25,7 +25,7 @@ function Roadmap() {
                     <h1 className=" opacity-100  z-30 sm:w-screen sm:top-8 sm:text-[2.8rem] sm:mx-[18%] md:w-screen md:top-12 md:text-[5rem] md:mx-[20%] lg:w-screen  absolute lg:top-20 lg:text-[6rem] lg:mx-[29.2%] xl:top-20 2xl:top-0 2xl:mx-[30.2%] 2xl:mt-40 xl:w-screen    xl:mx-[34%] batsand xl:text-[7rem] 2xl:w-screen 2xl:text-[11rem]"> THE TURTLES <br /> &nbsp; ROAD MAP   </h1>
                     <h1 className=" opacity-100 z-20  sm:w-screen sm:top-8 sm:text-[2.8rem] sm:mx-[17.2%] md:w-screen md:top-12 md:text-[5rem] md:mx-[19.2%] absolute lg:w-screen lg:top-20 lg:text-[6rem] lg:mx-[28.5%] xl:top-20  2xl:mx-[29.8%] 2xl:mt-40 2xl:top-4 xl:w-screen  xl:mx-[33%] batsand-dark xl:text-[7rem] 2xl:w-screen 2xl:text-[11rem]"> THE TURTLES <br /> &nbsp;  ROAD MAP   </h1>
          
-                    <div id="roadmap" className="absolute box-1 sm:w-[80%] sm:h-[13.4%] sm:mx-[4%] sm:top-[-25%] md:w-[70%] md:mx-[15%] md:h-[10%] md:top-[25%]   border lg:w-[70%] lg:h-[10%] lg:top-[25%] lg:mx-[17%] xl:w-[70%] 2xl:w-[70%] 2xl:h-[10%] 2xl:top-[22%] 2xl:mt-40 2xl:mx-[15.9%] xl:h-[10%] xl:top-[29.2%]  div-road   z-10   xl:mx-[13.9%]">
+                    <div id="roadmap" className="absolute box-1 sm:w-[80%] sm:h-[13.4%] sm:mx-[10%] sm:top-[25%] md:w-[70%] md:mx-[15%] md:h-[10%] md:top-[25%]   border lg:w-[70%] lg:h-[10%] lg:top-[25%] lg:mx-[17%] xl:w-[70%] 2xl:w-[70%] 2xl:h-[10%] 2xl:top-[22%] 2xl:mt-40 2xl:mx-[15.9%] xl:h-[10%] xl:top-[29.2%]  div-road   z-10   xl:mx-[13.9%]">
         
                         
                                 <div className=" sm:text-center md:text-center xl:text-center 2xl:text-center lg:text-center  ">
@@ -40,7 +40,7 @@ function Roadmap() {
                     {/* 2nd box */}
         
                     <div className="">
-                            <img className=" opacity-100 z-100 xl:my-[0.01%] sm:my-[-35%]  " src="./assets/texture-2.png" alt="texture" />
+                            <img className=" opacity-100 z-100 xl:my-[0.01%]   " src="./assets/texture-2.png" alt="texture" />
                            
                     <div className="absolute sm:w-[80%] sm:h-[13.4%] sm:mx-[10%] sm:top-[43%]  box-1 md:w-[70%] md:mx-[15%] md:h-[10%] md:top-[40%]  border lg:w-[70%] lg:top-[40%] lg:h-[10%] lg:mx-[17%] xl:w-[70%] 2xl:w-[70%] 2xl:h-[10%] 2xl:top-[38%] 2xl:mt-40 2xl:mx-[15.9%] xl:h-[10%] xl:top-[46%]  opactiy-80 div-road  xl:mx-[13.9%]">
                
@@ -61,10 +61,10 @@ function Roadmap() {
                            
         
           <div className=" ">
-                <img className=" opacity-100 z-100 xl:my-[0.01%] sm:my-[35%] sm:h-[20%]  " src="./assets/curve-texture.png" alt="texture" />
+                <img className=" opacity-100 z-100 xl:my-[0.01%]  " src="./assets/curve-texture.png" alt="texture" />
                            
                            
-                    <div className="absolute sm:w-[80%] sm:h-[13.4%] sm:mx-[10%] sm:top-[63%]   box-1 md:w-[70%] md:mx-[15%] md:h-[10%] md:top-[55%] lg:w-[70%] lg:top-[55%] lg:mx-[17%] lg:h-[10%]  border xl:w-[70%] 2xl:w-[70-%] 2xl:h-[10%] 2xl:top-[53.5%] 2xl:mt-40 2xl:mx-[15.9%] xl:h-[10%] xl:top-[62%]  opactiy-80 div-road  xl:mx-[13.9%]">
+                    <div className="absolute sm:w-[80%] sm:h-[13.4%] sm:mx-[10%] sm:top-[61%]   box-1 md:w-[70%] md:mx-[15%] md:h-[10%] md:top-[55%] lg:w-[70%] lg:top-[55%] lg:mx-[17%] lg:h-[10%]  border xl:w-[70%] 2xl:w-[70-%] 2xl:h-[10%] 2xl:top-[53.5%] 2xl:mt-40 2xl:mx-[15.9%] xl:h-[10%] xl:top-[62%]  opactiy-80 div-road  xl:mx-[13.9%]">
                
                         
         <div className=" sm:text-center md:text-center xl:text-center lg:text-center 2xl:text-center ">
@@ -79,7 +79,20 @@ function Roadmap() {
         </div>
         
         
+           {/* 4th box */}
         
+           <div className="absolute sm:w-[80%] sm:h-[13.4%] sm:mx-[10%] sm:top-[78%]   box-1 md:w-[70%] md:mx-[15%] md:h-[10%] md:top-[55%] lg:w-[70%] lg:top-[55%] lg:mx-[17%] lg:h-[10%]  border xl:w-[70%] 2xl:w-[70-%] 2xl:h-[10%] 2xl:top-[53.5%] 2xl:mt-40 2xl:mx-[15.9%] xl:h-[10%] xl:top-[62%]  opactiy-80 div-road  xl:mx-[13.9%]">
+               
+                        
+               <div className=" sm:text-center md:text-center xl:text-center lg:text-center 2xl:text-center ">
+               
+                      
+               <h4 className="dummy-1 sm:text-[0.9rem] sm:-my-[3%] md:text-[1.3rem]  xl:my-[2%] 2xl:my-[4%]">dummy text</h4>
+                                               <h6 className="inner-dummy sm:text-[0.5rem] sm:-my-[6%] sm:leading-3 md:text-[0.8rem] md:leading-3 lg:text-[1rem]"> Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text Dummy text </h6>
+               </div>
+               
+               
+               </div>
               
         
 
