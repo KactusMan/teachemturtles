39ef487d30f836fbf83420c0c1277be15a4f72c9
rayan <rayanidrees4@gmail.com>
2023-02-03 23:37:28 +0500

    another menu push testing 4.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
 
   theme: {
     screens: {
-      'sm': {'min': '350px', 'max': '767px'},
+      'sm': {'min': '320px', 'max': '767px'},
       // => @media (min-width: 640px and max-width: 767px) { ... }
 
       'md': {'min': '768px', 'max': '1023px'},
