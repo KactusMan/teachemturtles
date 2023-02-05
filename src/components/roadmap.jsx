dbe9996244efd6bc9e329b98270557fd82b0d447
rayan <rayanidrees4@gmail.com>
2023-02-05 22:45:56 +0500

    test 3 on mob

--- src/components/roadmap.jsx
+++ src/components/roadmap.jsx
@@ -6,11 +6,11 @@ function Roadmap() {
         return (
         
                     
-                    <><div className=" sm:w-screen  md:w-[100%] min-[350px]:height-[223%] max-[450px]:height-[223%]      xl:w-[100%]   min-[1280px]:height-[200%] max-[1366px]:height-[300%] lg:w-[100%] 2xl:w-[100%]   ">
+                    <><div className=" sm:w-screen  md:w-[100%]       xl:w-[100%]   min-[1280px]:height-[200%] max-[1366px]:height-[300%] lg:w-[100%] 2xl:w-[100%]   ">
         
         
         
-                <div className="   absolute roadmap-bg sm:h-[120%] sm:w-screen  md:w-screen xl:w-screen lg:w-screen  2xl:h-[320%] ">
+                <div className="   absolute roadmap-bg sm:h-[100%] sm:w-screen  md:w-screen xl:w-screen lg:w-screen  2xl:h-[320%] ">
                     <img className="  opacity-100 z-50 sm:h-[50%]  " src="./assets/texture-1.png" alt="texture" />
                     <img className=" sm:w-[35%] sm:-my-[80%] absolute md:-z-0 md:-my-[35%] md:w-[35%] lg:-z-0 lg:-my-[35%] lg:w-[36%] xl:-z-0 xl:-my-[30%] 2xl:-z-0 2xl:-my-[34%] 2xl:w-[40%] " src="./assets/left-tree.png" alt="left-tree" />
                     <img className=" sm:w-[40%] sm:-my-[60%] sm:mx-[65%] absolute md:-z-0 md:mx-[60%] md:w-[40%] md:-my-[10%] lg:-z-0 lg:-my-[20%] lg:mx-[60%] lg:w-[40%] xl:-z-0  xl:mx-[60%] xl:w-[40%] xl:-my-[-4%] 2xl:-z-0 2xl:mx-[54%] 2xl:my-[-13%] 2xl:w-[46%] " src="./assets/right-tree.png" alt="right-tree" />
@@ -19,7 +19,7 @@ function Roadmap() {
                     <img className="sm:w-[15%] sm:bg-cover sm:right-0 sm:top-[-12%] absolute md:top-[-14%] md:right-0 md:w-[14%] md:-z-0 lg:-z-0 lg:top-[-18%] lg:w-[16%] lg:-right-0 xl:-z-0 xl:right-0 xl:top-[-18%] 2xl:-z-0 2xl:right-0 2xl:top-[-14%] 2xl:w-[15%]" src="./assets/right-boulder.png" alt="right-boulder" />
                     
                     <img className=" sm:w-[7%] sm:top-[11%] sm:right-6 absolute md:right-14 md:w-[7%] md:top-[12%] lg:right-6 lg:top-[16%]  xl:-z-0 xl:right-6 xl:top-[13%] 2xl:right-6 2xl:top-[13%] 2xl:w-[6%]" src="./assets/pebble-right.png" alt="right-pebble" />
-                    <img className=" sm:w-[12%] sm:top-[9%] sm:left-4 absolute md:left-6 md:w-[9%] md:top-[14%] lg:left-6 lg:top-[13%] xl:-z-0 xl:left-6 xl:top-[10%] 2xl:left-6 2xl:top-[10%] 2xl:w-[6%]" src="./assets/pebble-left.png" alt="left-pebble" />
+                    <img className=" sm:w-[12%] sm:top-[7%] sm:left-4 absolute md:left-6 md:w-[9%] md:top-[14%] lg:left-6 lg:top-[13%] xl:-z-0 xl:left-6 xl:top-[10%] 2xl:left-6 2xl:top-[10%] 2xl:w-[6%]" src="./assets/pebble-left.png" alt="left-pebble" />
                     
 
                     <h1 className=" opacity-100  z-30 sm:w-screen sm:top-8 sm:text-[2.8rem] sm:mx-[23%] md:w-screen md:top-12 md:text-[5rem] md:mx-[20%] lg:w-screen  absolute lg:top-20 lg:text-[6rem] lg:mx-[29.2%] xl:top-20 2xl:top-0 2xl:mx-[30.2%] 2xl:mt-40 xl:w-screen    xl:mx-[34%] batsand xl:text-[7rem] 2xl:w-screen 2xl:text-[11rem]"> THE TURTLES <br /> &nbsp; ROAD MAP   </h1>
