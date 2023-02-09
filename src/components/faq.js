c7d4ed106dc00dd7733f81a1b45b1e5466f93e6e
unknown <rayanidrees4@gmail.com>
2023-02-09 14:17:10 -0800

    mobile changes with faq workingg

---
+++ src/components/faq.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import parse from "html-react-parser";
+import { Collapsible } from "collapsible-react-component";
+import "collapsible-react-component/dist/index.css";
+
+const Faq = ({ question, answer }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+        <div
+          className="  w-full py-6 mb-5 p-2 faq-bg rounded-lg ">
+          <div className="  shadow-[0px 4px 24px rgba(0, 0, 0, 0.25)]  ">
+            <div>
+              <div className="flex   justify-between items-center   pb-2">
+                <div className="">
+                  <p className="text-[17px] faq-question md:text-[20px] font-light md:font-bold leading-[22px] text-white ml-8 ">
+                    {question}
+                  </p>
+                </div>
+                <div className="mr-10">
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setOpen(!open);
+                    }}
+                  >
+                    {open ? (
+                      <img src="./assets/arrup.png" alt="arrup " />
+                    ) : (
+                      <img src="./assets/arrdown.png" alt="arrdown " />
+                    )}
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div>
+              <Collapsible open={open}>
+                <div>
+                  <div className="flex justify-between items-center   ">
+                    <div className="mx-8 ">
+                      <p className="text-sm font-light md:text-[17px] text-[#969696] md:text-white md:font-normal leading-6">
+                        {parse(answer)}</p>
+                    </div>
+                  </div>
+                </div>
+              </Collapsible>
+            </div>
+          </div>
+        </div>
+  );
+};
+
+export default Faq;
---
+++ src/components/faq_data.js
@@ -0,0 +1,57 @@
+export const FaqData = [
+    {
+      question: "Q:  What is Superwall?",
+      answer:
+        " Superwall is a Social Media NFT Platform built to bring Entertainment, Branding, Marketing & Content Production all under one roof. We want to empower our Content Creators (Supers) with the ability to monetize their exclusive contents by creating an Entertain 2 Earn(E2E) & Play 2 Earn(P2E) ecosystem. Through the ecosystems we look to bridge Web 2.0 users to Web 3.0 so the transitioning may be smooth. <br/> <a class=cursor-pointer   target=_blank href=https://Superwall.io>https://Superwall.io</a>",
+    },
+    {
+      question: "Q:  What is Super Pass?",
+      answer:
+        "Super Pass is a collection of 1,111 Genesis Passes built on the Ethereum Network, which will grant users exclusive benefits and access to the Superwall Ecosystem. Join us on our journey in becoming one of the leading pioneers that strive to build the future of Web3 Entertainment Brick By Brick. <br/> <span class=font-bold> - Super Pass - </span> <br/>  Total Supply: 1111 <br/>  Private: 100 OG: 300 <br/>  WL: 600 <br/>  Team Vault: 55 <br/>  Marketing Vault: 56",
+    },
+    {
+      question: "Q:  When is Phase 1 mint? (UTC)",
+      answer:
+        " Phase 1: Supply - 250 </span> <br/> Stage 1 (Private) <br/> Stage 2 (OG 24hrs) December 15th, 2022 12:00 PM <br/>  Stage 3 (WL 24hrs) December 16th, 2022 12:00 PM <br/>  Stage 4 (Public until mint out)",
+    },
+    {
+      question: "Q:   What is mint price for Phase 1",
+      answer:
+        " Stage 1 (Private 72hrs) - 0.2 eth <br/>  Stage 2 (OG 24hrs) - 0.2 eth <br/>  Stage 3 (WL 24hrs) - 0.25 eth <br/>  Stage 4 (Public until mint out) - 0.3eth",
+    },
+    {
+      question: "Q:  When does the other Super Pass Mint Phases Start?",
+      answer:
+        "Future Super Pass Mint Phases are TBA. <br/>  To stay up-to-date on future minting phases, Join us on Discord or follow us on Twitter",
+    },
+    {
+      question: "Q: Am I guaranteed to mint?",
+      answer:
+        "Stage 1 - Guaranteed Mint for Private Sale Wallets. <br/>  Stage 2 - Guaranteed Mint for OG 1 per Wallet Submissions. <br/>  Stage 3 - First Come First Serve 1 per Wallet submitted. <br/>  Stage 4 - If there are remaining supplies for Phase 1, First Come First Serve Mint basis, No Cap on minting. <br/>  If you didn’t get to mint during Super Pass Phase 1 Mint, fret not, you still have the chance to mint during Phase 2, Phase 3 or Phase 4!",
+    },
+    {
+      question: "Q: Will I be able to mint from mobile?",
+      answer:
+        "Yes. If you are facing issues, please contact our team via opening a support ticket on Discord.",
+    },
+    {
+      question: "Q: Is the contract gas optimized?",
+      answer: "Yes. The contract is built on ERC721A.",
+    },
+    {
+      question: "Q: How do I know if my wallet is registered?",
+      answer:
+        "Please create a ticket and our team can verify whether or not your wallet has been registered. If our ticketing system is at capacity, please wait and try again later.",
+    },
+    {
+      question: "Q: Is the Team Doxxed?",
+      answer:
+        "Yes. Key core members involved in the project at superwall are fully doxxed.",
+    },
+    {
+      question: "Q: How will the funds raised from the Super Pass be used?",
+      answer:
+        "1. Genesis Elemental Bricks - 20% <br/>  2. Superwall Platform Development - 20% <br/>  3. Marketing - 20% <br/>  4. Overhead Expenses - 20% <br/>  5. Team Compensation - 20%",
+    },
+  ];
+  
