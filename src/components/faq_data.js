03a9836bd2135566b9f99db86fd6e1caf3f571eb
unknown <rayanidrees4@gmail.com>
2023-02-12 05:50:46 +0500

    all done for everything

--- src/components/faq_data.js
+++ src/components/faq_data.js
@@ -1,57 +1,57 @@
 export const FaqData = [
     {
-      question: "Q:  What is Superwall?",
+      question: "Q:  Lorem Imsump?",
       answer:
-        " Superwall is a Social Media NFT Platform built to bring Entertainment, Branding, Marketing & Content Production all under one roof. We want to empower our Content Creators (Supers) with the ability to monetize their exclusive contents by creating an Entertain 2 Earn(E2E) & Play 2 Earn(P2E) ecosystem. Through the ecosystems we look to bridge Web 2.0 users to Web 3.0 so the transitioning may be smooth. <br/> <a class=cursor-pointer   target=_blank href=https://Superwall.io>https://Superwall.io</a>",
+        " Lorem Imsump Lorem Imsump Lorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem Imsump</a>",
     },
     {
-      question: "Q:  What is Super Pass?",
+      question: "Q:  Lorem Imsump",
       answer:
-        "Super Pass is a collection of 1,111 Genesis Passes built on the Ethereum Network, which will grant users exclusive benefits and access to the Superwall Ecosystem. Join us on our journey in becoming one of the leading pioneers that strive to build the future of Web3 Entertainment Brick By Brick. <br/> <span class=font-bold> - Super Pass - </span> <br/>  Total Supply: 1111 <br/>  Private: 100 OG: 300 <br/>  WL: 600 <br/>  Team Vault: 55 <br/>  Marketing Vault: 56",
+        "Lorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem Imsump",
     },
     {
-      question: "Q:  When is Phase 1 mint? (UTC)",
+      question: "Q:  Lorem Imsump",
       answer:
-        " Phase 1: Supply - 250 </span> <br/> Stage 1 (Private) <br/> Stage 2 (OG 24hrs) December 15th, 2022 12:00 PM <br/>  Stage 3 (WL 24hrs) December 16th, 2022 12:00 PM <br/>  Stage 4 (Public until mint out)",
+        " Lorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem Imsump",
     },
     {
-      question: "Q:   What is mint price for Phase 1",
+      question: "Q:   Lorem Imsump",
       answer:
-        " Stage 1 (Private 72hrs) - 0.2 eth <br/>  Stage 2 (OG 24hrs) - 0.2 eth <br/>  Stage 3 (WL 24hrs) - 0.25 eth <br/>  Stage 4 (Public until mint out) - 0.3eth",
+        " Lorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem Imsump",
     },
     {
-      question: "Q:  When does the other Super Pass Mint Phases Start?",
+      question: "Q:  Lorem Imsump",
       answer:
-        "Future Super Pass Mint Phases are TBA. <br/>  To stay up-to-date on future minting phases, Join us on Discord or follow us on Twitter",
+        "Lorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem Imsump",
     },
     {
-      question: "Q: Am I guaranteed to mint?",
+      question: "Q: Lorem Imsump",
       answer:
-        "Stage 1 - Guaranteed Mint for Private Sale Wallets. <br/>  Stage 2 - Guaranteed Mint for OG 1 per Wallet Submissions. <br/>  Stage 3 - First Come First Serve 1 per Wallet submitted. <br/>  Stage 4 - If there are remaining supplies for Phase 1, First Come First Serve Mint basis, No Cap on minting. <br/>  If you didn’t get to mint during Super Pass Phase 1 Mint, fret not, you still have the chance to mint during Phase 2, Phase 3 or Phase 4!",
+        "Lorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem Imsump",
     },
     {
-      question: "Q: Will I be able to mint from mobile?",
+      question: "Q: Lorem Imsump",
       answer:
-        "Yes. If you are facing issues, please contact our team via opening a support ticket on Discord.",
+        "Lorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem Imsump",
     },
     {
-      question: "Q: Is the contract gas optimized?",
-      answer: "Yes. The contract is built on ERC721A.",
+      question: "Q: Lorem Imsump",
+      answer: "Lorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem Imsump",
     },
     {
-      question: "Q: How do I know if my wallet is registered?",
+      question: "Q: Lorem Imsump",
       answer:
-        "Please create a ticket and our team can verify whether or not your wallet has been registered. If our ticketing system is at capacity, please wait and try again later.",
+        "Lorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem Imsump",
     },
     {
-      question: "Q: Is the Team Doxxed?",
+      question: "Q: Lorem Imsump",
       answer:
-        "Yes. Key core members involved in the project at superwall are fully doxxed.",
+        "Lorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem Imsump",
     },
     {
-      question: "Q: How will the funds raised from the Super Pass be used?",
+      question: "Q: Lorem Imsump",
       answer:
-        "1. Genesis Elemental Bricks - 20% <br/>  2. Superwall Platform Development - 20% <br/>  3. Marketing - 20% <br/>  4. Overhead Expenses - 20% <br/>  5. Team Compensation - 20%",
+        "Lorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem ImsumpLorem Imsump",
     },
   ];
   
